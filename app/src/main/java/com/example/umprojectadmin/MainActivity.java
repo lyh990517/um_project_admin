884ade706a91cc0829167f1a5d16865e8f8ac158
dsk0903 <dsk0903@daum.net>
2023-05-28 00:06:28 +0900

    user_list 추가

--- app/src/main/java/com/example/umprojectadmin/MainActivity.java
+++ app/src/main/java/com/example/umprojectadmin/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.umprojectadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button usr_list_button = (Button) findViewById(R.id.btn_user_list);
+        usr_list_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/umprojectadmin/UserList.java
@@ -0,0 +1,14 @@
+package com.example.umprojectadmin;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_list);
+    }
+}
